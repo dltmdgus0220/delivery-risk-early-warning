33c9d0bb2819437c5390e64dfe5f3ffaa4be56a0
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2026-01-07 23:59:09 +0900

    feat: 명사 추출 함수 생성 및 불용어 정의

--- src/keyword_extract_bertopic.py
+++ src/keyword_extract_bertopic.py
@@ -3,3 +3,16 @@ from bertopic import BERTopic
 from kiwipiepy import Kiwi
 from sklearn.feature_extraction.text import CountVectorizer
 from sentence_transformers import SentenceTransformer
+
+# --- 1. 명사 추출 및 불용어 정의---
+
+STOPWORDS = []
+kiwi = Kiwi()
+
+def extract_nouns(text):
+    if not isinstance(text, str): 
+        return ""
+    result = kiwi.tokenize(text)
+    # NNG(일반명사), NNP(고유명사), XR(어근) 추출
+    return " ".join([t.form for t in result if t.tag in ['NNG', 'NNP', 'XR'] and len(t.form) > 1])
+

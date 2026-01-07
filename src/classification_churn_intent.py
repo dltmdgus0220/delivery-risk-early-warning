56522d2b2bc593631261df05b1db8a1f221d974e
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2026-01-07 13:56:59 +0900

    feat: 환경설정

--- src/classification_churn_intent.py
+++ src/classification_churn_intent.py
@@ -14,3 +14,31 @@ from sklearn.metrics import accuracy_score, f1_score
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from tqdm import tqdm
 
+
+# --- 0. 설정 ---
+
+SEED = 42
+MODEL_ID = "kykim/bert-kor-base"  # klue/bert-base, 참고:https://github.com/kiyoungkim1/LMkor
+TRAIN_CSV_PATH = "data/out.csv"
+CSV_PATH = "data/baemin_reviews_playstore_100000.csv"
+
+TEXT_COL = "content"
+LABEL_COL = "churn_intent_label"
+
+MAX_LEN = 128
+BATCH_SIZE = 16
+EPOCHS = 3
+LR = 2e-5 # BERT에서 거의 표준으로 사용하는 학습률
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu" # gpu를 위한 코드
+
+id2label = {0: "없음", 1: "약함", 2: "강함"}
+
+def set_seed(seed=42):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+
+os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "1" # 경고 안뜨게 하기
+

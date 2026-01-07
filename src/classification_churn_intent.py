c297e790c76ea63c5b96673a47c6a753a9773981
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2026-01-07 14:02:18 +0900

    feat: 학습 데이터셋 클래스 생성

--- src/classification_churn_intent.py
+++ src/classification_churn_intent.py
@@ -73,3 +73,38 @@ def load_and_prepare(csv_path: str) -> pd.DataFrame:
 
     return df
 
+
+# --- 2. 데이터셋 클래스 ---
+
+class TrainTextDataset(Dataset):
+    def __init__(self, df: pd.DataFrame, tokenizer, max_len: int):
+        self.texts = df[TEXT_COL].tolist()
+        self.labels = df["label"].astype(int).tolist()
+        self.confidences = df['churn_intent_confidence'].astype(float).tolist()
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx: int):
+        text = self.texts[idx]
+        label = self.labels[idx]
+        confidence = self.confidences[idx]
+
+        enc = self.tokenizer(
+            text,
+            truncation=True, # max_len보다 길면 뒷부분 삭제
+            padding="max_length", # max_len으로 패딩해서 길이 맞추기
+            max_length=self.max_len,
+            return_tensors="pt", # pytorch tensor 형태로 결과 리턴
+        )
+
+        item = {
+            "input_ids": enc["input_ids"].squeeze(0), # (1, MAX_LEN) -> (MAX_LEN,)
+            "attention_mask": enc["attention_mask"].squeeze(0), # (1, MAX_LEN) -> (MAX_LEN,)
+            "labels": torch.tensor(label, dtype=torch.long),
+            "confidences": torch.tensor(confidence, dtype=torch.float)
+        }
+        return item
+

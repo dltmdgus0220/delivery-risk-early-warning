fadfa119ca5161c7e5939c132a9971a5f1b98adc
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2026-01-07 14:02:44 +0900

    feat: 추론 데이터셋 클래스 생성

--- src/classification_churn_intent.py
+++ src/classification_churn_intent.py
@@ -108,3 +108,30 @@ class TrainTextDataset(Dataset):
         }
         return item
 
+
+class InferTextDataset(Dataset):
+    def __init__(self, df: pd.DataFrame, tokenizer, max_len: int):
+        self.texts = df[TEXT_COL].tolist()
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx: int):
+        text = self.texts[idx]
+
+        enc = self.tokenizer(
+            text,
+            truncation=True, # max_len보다 길면 뒷부분 삭제
+            padding="max_length", # max_len으로 패딩해서 길이 맞추기
+            max_length=self.max_len,
+            return_tensors="pt", # pytorch tensor 형태로 결과 리턴
+        )
+
+        item = {
+            "input_ids": enc["input_ids"].squeeze(0), # (1, MAX_LEN) -> (MAX_LEN,)
+            "attention_mask": enc["attention_mask"].squeeze(0), # (1, MAX_LEN) -> (MAX_LEN,)
+        }
+        return item
+

7477eff5d3f6d75a1a8fc41852980b1b994adcc0
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2026-01-07 23:58:45 +0900

    Create keyword_extract_bertopic.py

---
+++ src/keyword_extract_bertopic.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from bertopic import BERTopic
+from kiwipiepy import Kiwi
+from sklearn.feature_extraction.text import CountVectorizer
+from sentence_transformers import SentenceTransformer

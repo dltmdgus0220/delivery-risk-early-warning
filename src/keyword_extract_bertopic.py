adffe5a6cf51db354ef701d10e790111ae5cbe9c
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2026-01-09 00:26:00 +0900

    refactor: 키워드 도출 품질 개선

--- src/keyword_extract_bertopic.py
+++ src/keyword_extract_bertopic.py
@@ -3,6 +3,7 @@ from bertopic import BERTopic
 from kiwipiepy import Kiwi
 from sklearn.feature_extraction.text import CountVectorizer
 from sentence_transformers import SentenceTransformer
+from bertopic.representation import MaximalMarginalRelevance
 
 # --- 1. 명사 추출 및 불용어 정의---
 
@@ -30,7 +31,7 @@ df_copy['cleaned_text'] = df_copy['content'].apply(extract_nouns)
 sentence_model = SentenceTransformer("kimseongsan/ko-sbert-384-reduced") # 문장 전체 임베딩 : 문장 내 단어들의 관계를 고려하여 각 문장마다 하나의 임베딩벡터 만들어줌.
 
 # 키워드 추출 시 불용어 제거를 위한 Vectorizer 설정
-vectorizer_model = CountVectorizer(stop_words=STOPWORDS, min_df=10, max_df=0.95) # min_df: 최소빈도, max_df: 최대빈도
+vectorizer_model = CountVectorizer(stop_words=STOPWORDS, min_df=2) # min_df: 최소빈도, max_df: 최대빈도
 
 
 # --- 4. BERTopic 모델 생성 및 학습 ---
@@ -39,7 +40,8 @@ vectorizer_model = CountVectorizer(stop_words=STOPWORDS, min_df=10, max_df=0.95)
 topic_model = BERTopic(
     embedding_model=sentence_model,
     vectorizer_model=vectorizer_model,
-    nr_topics=20, # auto : 토픽 개수 자동 조절
+    representation_model = MaximalMarginalRelevance(diversity=0.3), # diversity가 0에 가까울수록 키워드 중복이 줄어듦
+    nr_topics="auto", # auto : 토픽 개수 자동 조절
     verbose=True, # 진행바 활성
     low_memory=True, # 메모리 절약 모드
     # calculate_probabilities=False # 메모리 부족 방지를 위해 False 설정

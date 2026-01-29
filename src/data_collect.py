06ab00673631d09bef05e99f7708a4c2bfaf9c9d
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2026-01-29 17:22:50 +0900

    feat: 기간 기준 데이터수집 함수 구현

--- src/data_collect.py
+++ src/data_collect.py
@@ -60,3 +60,56 @@ def collect_reviews_by_num(app_id, num: int=1000, lang="ko", country="kr", batch
 
     return list_to_df(all_reviews)
 
+
+def collect_reviews_by_date(app_id, start_date, end_date=None, lang="ko", country="kr", batch_size=200, sleep_sec=0.2):
+    all_reviews = []
+    continuation_token = None
+    seen = set()
+
+    start_date = start_date.replace(tzinfo=None)
+    if end_date:
+        end_date = end_date.replace(tzinfo=None)
+
+    while True:
+        result, continuation_token = reviews(
+            app_id,
+            lang=lang,
+            country=country,
+            sort=Sort.NEWEST,
+            count=batch_size,
+            continuation_token=continuation_token
+        )
+
+        if not result:
+            break
+
+        stop = False
+
+        for r in result:
+            review_dt = r["at"].replace(tzinfo=None) # timezone 제거
+
+            # end_date가 있으면 end_date 이후 리뷰는 제외
+            if end_date and review_dt > end_date:
+                continue
+
+            # start_date보다 오래된 리뷰가 나오면 중단 조건
+            if review_dt < start_date:
+                stop = True
+                break
+            
+            # 중복 제거 (수집 중 리뷰가 추가되어 중복된 리뷰가 들어갈 수도 있음)
+            rid = r.get("reviewId")
+            if rid not in seen:
+                seen.add(rid)
+                all_reviews.append(r)
+
+        if stop:
+            break
+
+        if continuation_token is None:
+            break
+
+        time.sleep(sleep_sec)
+
+    return list_to_df(all_reviews)
+

ca342bf598118d7142daa0a3e58e93e05dd46708
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2026-01-29 14:03:43 +0900

    feat: 전역 시드 설정 함수 구현

--- src/classification/utils.py
+++ src/classification/utils.py
@@ -3,3 +3,12 @@ import numpy as np
 import pandas as pd
 import torch
 
+
+# 전역 시드 설정
+def set_seed(seed=42):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+

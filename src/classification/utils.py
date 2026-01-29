04f9e1ad1353cc42fd390092f08ded069170e607
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2026-01-29 14:04:00 +0900

    feat: 클래스 균등 추출 함수 구현

--- src/classification/utils.py
+++ src/classification/utils.py
@@ -12,3 +12,12 @@ def set_seed(seed=42):
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
 
+
+# 클래스 균등 추출
+def balanced_class_extract(df: pd.DataFrame, label: str, num: int, seed: int | None = None) -> pd.DataFrame:
+    balanced = (
+        df.groupby(label, group_keys=False)
+          .apply(lambda g: g.sample(n=num, random_state=seed), include_groups=False)
+    )
+
+    return balanced.sample(frac=1, random_state=seed).reset_index(drop=True) # 결과들을 다시 섞은 후 0부터 인덱스 정렬

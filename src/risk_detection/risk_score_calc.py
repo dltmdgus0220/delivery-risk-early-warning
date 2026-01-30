5a7159e967a8d7c8820bebeb54c4dc5290d4ebd1
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2026-01-30 16:54:00 +0900

    feat: 월별로 이탈 지수 계산 함수 구현

--- src/risk_detection/risk_score_calc.py
+++ src/risk_detection/risk_score_calc.py
@@ -6,3 +6,21 @@ def risk_score_calc(df):
     num2 = len(df[df['churn_intent_label'] == 2])
     return num2 * 1 + num1 * 0.9
 
+
+def monthly_risk_calc(df, date_col: str='at'):
+    # 날짜 컬럼 처리
+    df[date_col] = pd.to_datetime(df[date_col], errors="coerce") # errors='coerce' : 날짜변환 실패시 결측값 저장
+    df = df.dropna(subset=[date_col])
+
+    # 월별 컬럼 생성
+    df["month"] = df[date_col].dt.to_period("M").astype(str)
+
+    # 월별로 묶어서 이탈지수 계산
+    monthly_risk = (
+        df.groupby("month")
+        .apply(risk_score_calc, include_groups=False)
+        .reset_index(name="risk_score")
+    )
+
+    return monthly_risk
+

4222b55fcd964d595268729351e45003485b5f8e
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2026-01-30 16:53:44 +0900

    feat: 이탈 지수 계산 함수 구현

--- src/risk_detection/risk_score_calc.py
+++ src/risk_detection/risk_score_calc.py
@@ -1,2 +1,8 @@
 import pandas as pd
 
+
+def risk_score_calc(df):
+    num1 = len(df[df['churn_intent_label'] == 1])
+    num2 = len(df[df['churn_intent_label'] == 2])
+    return num2 * 1 + num1 * 0.9
+

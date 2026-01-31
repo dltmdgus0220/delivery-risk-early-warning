779d803042e30c16b39f0f7bd49f01190257fbcf
dltmdgus0220 <101940401+dltmdgus0220@users.noreply.github.com>
2026-01-31 10:42:49 +0900

    refactor: 이탈 지수 계산식 변경

--- src/risk_detection/risk_score_calc.py
+++ src/risk_detection/risk_score_calc.py
@@ -4,7 +4,7 @@ import pandas as pd
 def risk_score_calc(df):
     num1 = len(df[df['churn_intent_label'] == 1])
     num2 = len(df[df['churn_intent_label'] == 2])
-    return round((num2 * 1 + num1 * 0.9) / len(df), 2)
+    return round((num2 * 1 + num1 * 0.75) * (len(df)/(len(df)+60)) , 2)
 
 
 def monthly_risk_calc(df, date_col: str='at'):
